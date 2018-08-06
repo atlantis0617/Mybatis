458aeb1bc0e4d2f6b22d9c3adc9b8cc34c924266
atlantis0617 <1636720042@qq.com>
2018-08-06 16:18:57 +0800

    项目初始化

---
+++ src/main/java/com/www/yhy/domain/User.java
@@ -0,0 +1,57 @@
+package com.www.yhy.domain;
+
+/** 
+* @ClassName: User 
+* @Description: 
+* @author atlantis0617
+* @date 2018年8月6日 下午4:06:04 
+*  
+*/
+public class User {
+	
+	private int id;
+	
+	private String name;
+	
+	private int age;
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	 public String toString() {
+	  return "User [id=" + id + ", name=" + name + ", age=" + age + "]";
+	 }
+
+}
